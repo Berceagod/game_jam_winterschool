4b59d0d94989b6792c1467948cdb3f0f9bf4cc0f
NicaOct <octaviannica301@gmail.com>
2025-02-19 23:51:12 +0200

    light0.2

---
+++ WinterSchool GameJam/Assets/Scripts/UI/Shhhhhhhh.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Shhhhhhhh : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Shhhhh");
+    }
+}
